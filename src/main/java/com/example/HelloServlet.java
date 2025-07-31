9181615f9c8f3ae20133b734cd60a4aa2971f4a4
Bui Duc <buiducvietdz2005@gmail.com>
2025-07-31 13:19:20 +0700

    d

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -16,7 +16,7 @@ public class HelloServlet extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
-        out.println("<h1>Hello, World,  am a servlet, chú ý nhánh của Trần Huy Hoàng-29-07-2025</h1>");
+        out.println("<h1>Hello, World, I am a servlet, chú ý nhánh của Trần Huy Hoàng-29-07-2025</h1>");
         out.println("<a href='http://localhost:8082/java-servlet-web-1.0-SNAPSHOT/'><button>Back to Home</button></a>");
     }
 }
